b7ac265d0d9fd518ec70136bb32d7a2cc274141f
Alejandro Galeano <ale.gb32@gmail.com>
2020-08-09 21:39:48 -0300

    Requerida la instancia de la base de datos

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+//librerias requerias
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// Instancia de la aplicacion
 const app = express();
 
+require('./db');
 
+app.use(bodyParser.json());
+// para recibir peticiones post
+app.use(bodyParser.urlencoded({extended: true}));
+
+//ruta raiz
 app.get('/', (req, res)=>{
     res.send('hola mundo');
 })
 
+//Servidor escuchando
 app.listen(3000,()=>{
     console.log('Servidor corriendo en puerto 3000');
 });
