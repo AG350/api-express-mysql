2672b847f2c84378e306916227b0773e78575834
Alejandro Galeano <ale.gb32@gmail.com>
2020-08-26 00:03:12 -0300

    Validacion de campos de registro

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const bcryp = require('bcryptjs');
 const { User } = require('../../db');
+const { check, validationResult } = require('express-validator');
 
-router.post('/register', async (req,res) =>{
+router.post('/register', [
+    check('username', 'El Nombre de usuario es obligatorio').not().isEmpty(),
+    check('password', 'El Password es obligatorio').not().isEmpty(),
+    check('email','Ingrese una direccion de correo valida').isEmail()
+] ,async (req,res) =>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json( {errores: errors.array()})
+    }
 
     req.body.password = bcryp.hashSync(req.body.password, 10);
     const user = await User.create(req.body);
